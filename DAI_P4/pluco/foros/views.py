ca00146ec0f352a779a1c5c014282ac3113e39b4
rafaellg8 <rafaellg8@correo.ugr.es>
2015-12-03 00:17:59 +0100

    genera titulos

--- DAI_P4/pluco/foros/views.py
+++ DAI_P4/pluco/foros/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.template import RequestContext
 from models import Comment,Forum,User
 from django.http import HttpResponse
-from plucoApp.forms import Forum
+from plucoApp.forms import Forums,Comments,UserForms
+from django.shortcuts import redirect
 
 def comment(request):
+    if request.method =="POST":
+        form = Comments(request.POST)
+        #validamos el formulario
+        if form.is_valid():
+            newComment = Comment(request.POST["theme"],request.POST["idComment"],request.POST["title"],request.POST["commentText"],request.POST["userName"],request.POST["url"])
+            return redirect('foros.views.details',pk=form.pk)
+        else:
+            form = Comment()
+
     return render(request,'comentarios.html')
 
 def forums(request):
-    form = Forum
-    return render(request,'foros.html', {'form': form})
+    if request.method =="POST":
+        form = Forums(request.POST)
+        #validamos el formulario
+        if form.is_valid():
+            newForum = Forum(request.POST["title"],request.POST["theme"],request.POST["asignature"])
+            return redirect('foros.views.details',pk=form.pk)
+    else:
+        form = Forum()
+    return render(request,'foros.html', {'form': form},context_instance=RequestContext(request))
--- DAI_P4/pluco/plucoApp/forms.py
+++ DAI_P4/pluco/plucoApp/forms.py
@@ -1,17 +1,41 @@
-from django import forms
-from .models import Comment,User,Forum
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','pluco.settings')
 
-class UserForm(forms.ModelForm):
+import django
+django.setup()
+
+from plucoApp.models import Forum,User,Comment
+from django import forms
+
+class UserForms(forms.ModelForm):
     class Meta:
         model = User
         fields = ('userName','name','firstName','secondName','email','password','birthday',)
 
-class Comment(forms.ModelForm):
+
+    def addUser(userName,name,firstName,secondName,birthday,email,password):
+          u = User.objects.get_or_create(userName=userName,name=name,firstName=firstName,
+                secondName=secondName,birthday=birthday,email=email,password=password)[0]
+          return u
+
+class Comments(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('theme','title','commentText','url',)
 
-class Forum(forms.ModelForm):
+    def addComment(forum,idC,tit,commentTxt,user,url):
+          com = Comment.objects.get_or_create(theme=forum,idComment=idC,title=tit,commentText=commentTxt,userName=user,url=url)[0]
+          return com
+
+class Forums(forms.ModelForm):
     class Meta:
         model = Forum
         fields = ('title','theme','asignature',)
+
+    def addForum(self,title,theme,asignature):
+                forum = Forum.objects.get_or_create(title=title,theme=theme,asignature=asignature)[0]
+                forum.save()
+                print ("Guardamos el foro")
+                return forum
